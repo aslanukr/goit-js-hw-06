ca5a86fe8fd8adc5f479929f59bf27e2d71472a4
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-02-25 00:40:49 +0200

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,8 +1,8 @@
 const categoriesEl = document.querySelectorAll(".item");
 
-console.log(`Number of categories: ${categoriesEl.length}`);
+console.log("Number of categories:", categoriesEl.length);
 
 categoriesEl.forEach((el) => {
-  console.log(`Category: ${el.firstElementChild.textContent}`);
-  console.log(`Elements: ${el.lastElementChild.children.length}`);
+  console.log("Category:", el.firstElementChild.textContent);
+  console.log("Elements:", el.lastElementChild.children.length);
 });
