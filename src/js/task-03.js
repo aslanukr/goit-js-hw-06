abcd3cfd21feacc9913dd161ef78d66830845c0a
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-02-26 22:16:59 +0200

    Update task-03.js

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,14 +1,32 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryMarkup = images
+  .map(
+    (image) =>
+      `<li class="image"><img src="${image.url}" alt="${image.alt}" width = 1024></li>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryEl.style.display = "flex";
+galleryEl.style.flexDirection = "column";
+galleryEl.style.gap = "20px";
+galleryEl.style.padding = "0";
+galleryEl.style.margin = "0";
+galleryEl.style.listStyle = "none";
