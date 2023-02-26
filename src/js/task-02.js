90f4f24e14d2fe5a37eebd1cdef556bcd63f00de
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-02-26 21:25:35 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+ingredients.map((item) => {
+  const listItemEl = document.createElement("li");
+  listItemEl.textContent = item;
+  listItemEl.classList.add("item");
+  ingredientsEl.append(listItemEl);
+});
