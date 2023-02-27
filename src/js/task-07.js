db4950558dce66869bf8b254c45da8652f054036
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-02-27 03:54:33 +0200

    Update task-07.js

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,7 +1,10 @@
 const sizeControlEl = document.querySelector("#font-size-control");
 
 const textEl = document.querySelector("#text");
+textEl.style.fontSize = `${sizeControlEl.value}px`;
 
 sizeControlEl.addEventListener("input", (event) => {
   textEl.style.fontSize = `${event.target.value}px`;
 });
+
+console.log(sizeControlEl.value);
