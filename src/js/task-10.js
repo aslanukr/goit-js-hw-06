e46def1081b6dbad381874aaef759d2b08f3400a
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-02-27 04:37:05 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,24 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputAmountEl = document.querySelector("input[type=number]");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
+
+function handleCreateBtn() {
+  const basicBoxSize = 30;
+
+  for (let i = 0; i < inputAmountEl.value; i += 1) {
+    let boxColor = getRandomHexColor();
+    let boxSize = i * 10 + basicBoxSize;
+    const boxMarkup = `<div style="background:${boxColor}; width:${boxSize}px; height:${boxSize}px" > </div>`;
+    boxesEl.insertAdjacentHTML("beforeend", boxMarkup);
+  }
+}
+
+createBtn.addEventListener("click", handleCreateBtn);
+destroyBtn.addEventListener("click", () => {
+  boxesEl.innerHTML = "";
+});
