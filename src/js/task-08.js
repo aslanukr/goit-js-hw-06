087fd73bd7af05fbc81e9c29408563ea8ac69a23
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-02-27 03:15:56 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector(".login-form");
 
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const { email, password } = event.target;
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповненими!");
+  }
+
+  const elements = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(elements);
+
+  event.target.reset();
+};
+
+formEl.addEventListener("submit", handleSubmit);
