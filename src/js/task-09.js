9dfd20ec1d5e9a287194cbbbe62b3224e16250fe
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-02-27 03:25:04 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const changeColorBtnEl = document.querySelector(".change-color");
+const colorNameEl = document.querySelector(".color");
+
+const handleColorBtnClick = (event) => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorNameEl.textContent = getRandomHexColor();
+};
+
+changeColorBtnEl.addEventListener("click", handleColorBtnClick);
